5b995af84331cc2ad401487a5bdc1501f4800b85
Zobaidul Kazi <153954612+zbkazi@users.noreply.github.com>
2024-08-21 01:10:55 +0000

    output: 'export'

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,9 @@ const nextConfig = {
     domains: ["github.com", "avatars.githubusercontent.com"],
   },
   experimental: {
-  }
+    // Add experimental features here if needed
+  },
+  output: 'export', // Correct configuration for static HTML export
 };
 
 export default nextConfig;
--- src/app/notice/[id]/page.tsx
+++ src/app/notice/[id]/page.tsx
@@ -1,3 +1,4 @@
+// src/app/notice/[id]/page.tsx
 "use client";
 import { useParams, useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
